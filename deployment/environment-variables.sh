df6e5f78e9ec8f4efdb5f6db45e4fe10d9c6bdb8
Shannon Wainright <shannonw@sscott.com>
2018-07-17 15:20:41 -0700

    CS-427 #comment added docker deployment

---
+++ deployment/environment-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+###############################################################################################################
+# Build Variables
+###############################################################################################################
+export POM_ARTIFACT_NAME=travis-poc
+# export POM_VERSION=$(python -c 'import xml.etree.ElementTree as ET; tree = ET.parse("pom.xml"); root = tree.getroot(); print root.find("{http://maven.apache.org/POM/4.0.0}version").text;')
+export JAR=${POM_ARTIFACT_NAME}-${POM_VERSION}.jar
+
+# Define the Docker build name.
+export APP_REPOSITORY=$TRAVIS_REPO_SLUG
+export APP_VERSION="${POM_VERSION}-${TRAVIS_COMMIT:(-6)}"
+export DOCKER_TAG_VERSION=$APP_REPOSITORY:$APP_VERSION
+export DOCKER_TAG_LATEST=$APP_REPOSITORY:latest
+export APP_VERSION=$APP_VERSION
+
---
+++ deployment/tools.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+set -e
+
+#HAS_RUN=/tmp/get-shared-tools-has-run
+#if [ ! -f "${HAS_RUN}" ]; then
+
+    HELM_VERSION="helm-v2.8.2-linux-amd64.tar.gz"
+    KUBECTL_VERSION="v1.8.12"
+    TOOLS_PATH=/tmp/tools
+
+    ###################################################################
+    # Start
+    ###################################################################
+    echo "Installing Prerequisites for build"
+
+    mkdir -p ${TOOLS_PATH}
+    cd ${TOOLS_PATH}
+
+
+    ###################################################################
+    # AWS
+    ###################################################################
+    if [ ! -x /usr/local/bin/aws ]; then
+        mkdir -p ${TOOLS_PATH}/aws-cli
+        pushd ${TOOLS_PATH}/aws-cli
+        if [ ! -d install ]; then
+            if [ ! -d awscli-bundle ]; then
+                echo "[+] Downloading AWS CLI"
+                curl -sSLO "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
+                unzip awscli-bundle.zip
+            fi
+
+            echo "[+] Installing AWS CLI"
+            time ./awscli-bundle/install -i $(pwd)/install -b ${TOOLS_PATH}/aws
+        fi
+        ln -s ${TOOLS_PATH}/aws /usr/local/bin/aws
+        popd
+    fi
+    aws --version
+
+    ###################################################################
+    # Kubernetes
+    ###################################################################
+    if [ ! -x /usr/local/bin/kubectl ]; then
+        if [ ! -x ${TOOLS_PATH}/kubectl ]; then
+            echo "[+] Downloading kubectl"
+            curl -sSLO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+            chmod +x kubectl
+        fi
+        cp ${TOOLS_PATH}/kubectl /usr/local/bin
+    fi
+
+    echo "[+] Configuring kubectl"
+    kubectl config set-cluster simon365 --server=$KUBERNETES_SERVER --insecure-skip-tls-verify=true
+    kubectl config set-credentials simon365-deployer --token=$KUBERNETES_TOKEN
+    kubectl config set-context bitbucket-pipeline-context --cluster=simon365 --user=simon365-deployer
+    kubectl config use-context bitbucket-pipeline-context
+
+    echo "[+] Verify connectivity"
+    kubectl version
+
+    ###################################################################
+    # HELM 
+    ###################################################################
+
+    if [ ! -x /usr/local/bin/helm ]; then
+        if [ ! -x ${TOOLS_PATH}/helm ]; then
+            echo "[+] Downloading helm"
+            curl -sSLO https://storage.googleapis.com/kubernetes-helm/${HELM_VERSION}
+            tar -xzf ${HELM_VERSION}
+            mv linux-amd64/* .
+            rm -rf linux-amd64
+        fi
+        cp ${TOOLS_PATH}/helm /usr/local/bin
+    fi
+    helm version
+
+    ###################################################################
+    # Done
+    ###################################################################
+    echo "Prerequisites completed."
+
+#    touch "${HAS_RUN}"
+#fi
