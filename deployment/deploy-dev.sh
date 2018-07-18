9913b388c15448392c1dcf041d87d86e9a930033
Shannon Wainright <shannonw@sscott.com>
2018-07-18 15:37:10 -0700

    CS-472 #comment helm update

--- deployment/deploy-dev.sh
+++ deployment/deploy-dev.sh
@@ -13,9 +13,11 @@ DEPLOY_NAME=${APP_REPOSITORY}-dev
 # Create the helm chart
 helm template ${TRAVIS_BUILD_DIR}/deployment/helm --set image=${ECR_REPO}/${DOCKER_TAG_VERSION} -f ${TRAVIS_BUILD_DIR}/deployment/helm/env/dev.yaml --name ${DEPLOY_NAME}
 
+echo; echo "[+] Deleting previous helm deployment ${DEPLOY_NAME}"; echo
 helm delete --purge \
         ${DEPLOY_NAME} || true
 
+echo; echo "[+] Installing dev environment deployment ${DEPLOY_NAME}"; echo
 helm install \
         --set image=${ECR_REPO}/${DOCKER_TAG_VERSION} \
         --namespace dev \
